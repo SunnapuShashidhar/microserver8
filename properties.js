378ca306acd3bd2bd1448fc768e2f750e8cc9f52
Shashidhar Sunnapu <sunnapushasidhar@gmail.com>
2022-08-24 12:18:25 +0530

    assignment7 ms is commiting

---
+++ properties.js
@@ -0,0 +1,4 @@
+module.exports = {
+   DB_URL: "mongodb+srv://admin:lPiosX47pLKBjpZz@shashidhar.32vvw8h.mongodb.net/restarentDB?retryWrites=true&w=majority"
+    // DB_URL:"mongodb://localhost:27017/restarentDB"
+}
